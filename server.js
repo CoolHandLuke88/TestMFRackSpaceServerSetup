f7f79d91040b1424c4dea4cc82e034a5943460c0
Luke@dsmedialabs.com <luke@dsmedialabs.com>
2014-01-16 20:16:56 -0500

    updates

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/plain'});
 res.end('Luke You Got This Mother Fucker to work ya biatch!!\n');
 
-}).listen(8125, "162.242.170.83");
+}).listen(8124, "162.242.170.83");
 
 console.log('listening to server at 162.242.170.83');
